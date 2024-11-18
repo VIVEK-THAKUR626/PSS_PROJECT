8e5905f42b3a73cca786399485b92e06b84efa8a
VIVEK-THAKUR626 <vistaar4324@gmail.com>
2024-11-18 17:14:59 +0530

    Add a feature to check pass/fail based on average score

--- student_grade_management.py
+++ student_grade_management.py
@@ -3,6 +3,7 @@ class marks:
         self.subject = subject
         self.score = 0
         self.no_subjects = 0
+        self.fail = False
     def input_marks_subject(self):
         self.no_subjects = int(input("Enter no. of subjects :"))
         self.subject_dict = dict()
@@ -13,7 +14,11 @@ class marks:
     def total_average(self):
         self.total = sum(self.subject_dict.values())
         self.average = self.total/self.no_subjects
-    
+    def status(self):
+        if self.average <= 16:
+            self.fail = True
+        self.performance = "FAIL" if self.fail else "PASS"
+
 class student(marks):
     def __init__(self,id,subject="",name = ""):
         super().__init__(subject)
@@ -28,9 +33,11 @@ class student(marks):
         print(f"Marks : {self.subject_dict}")
         print(f"Total marks : {self.total}")
         print(f"Average : {self.average}")
+        print(f"Status : {self.performance}")
     
 student1 = student(2411981626)
 student1.input_name()
 student1.input_marks_subject()
 student1.total_average()
+student1.status()
 student1.display()
